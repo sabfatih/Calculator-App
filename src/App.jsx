dd804bde699f81a236a787945cde9e2dba2698ee
Fatih <alpha5808r@gmail.com>
2025-04-03 22:49:10 +0700

    Almost finished, still have unfinished percentage logic and expand calculator feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
 import "./App.css";
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <>
-      <h1>This is header gng</h1>
+      <header className="text-center my-5">
+        <span className="text-5xl font-extrabold bg-gradient-to-l from-purple-400 to-fuchsia-400 bg-clip-text text-transparent">
+          Calculator
+        </span>
+        <p className="text-xl font-semibold text-purple-300">By Fatih</p>
+      </header>
+      <Calculator />
     </>
   );
 }
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,205 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Calculator = () => {
+  // const inputRef = useRef(null);
+
+  // useEffect(() => {
+  //   inputRef.current.focus();
+  // }, []);
+
+  const [inputUser, setInputUser] = useState("0");
+  const [result, setResult] = useState("0");
+
+  const checkCalculation = (params) => {
+    const checkPercentage = params.replace(/(\d+)%/, "");
+    const operators = ["+", "-", "*", "/"];
+
+    if (operators.includes(calculation.slice(-1))) {
+      const result = calculation.slice(0, -1);
+      return checkCalculation(result);
+    } else {
+      return calculation;
+    }
+  };
+
+  useEffect(() => {
+    const calculation = checkCalculation(
+      inputUser.replaceAll("×", "*").replaceAll("--", "+")
+    );
+    console.log(calculation);
+    const calculate = new Function(`return ${calculation}`)();
+
+    setResult(calculate);
+  }, [inputUser]);
+
+  return (
+    <div className="mx-4 px-3 py-4">
+      <div className="mx-auto w-72 rounded-md bg-purple-400 px-5 py-6">
+        {/* <label className="text-right flex flex-col pt-8 px-3">
+          <input
+            type="text"
+            ref={inputRef}
+            className="bg-none text-xl ml-auto outline-none text-neutral-800"
+          />
+          <p className="">123</p>
+        </label> 
+        */}
+
+        <div className="flex flex-col">
+          <p className="ml-auto text-2xl font-semibold">{inputUser}</p>
+          <p className="ml-auto text-xl">{result}</p>
+        </div>
+        <hr className="border-t-1 my-1" />
+        {/* Buttons */}
+        <div className="mt-3 grid grid-cols-4 grid-rows-5 gap-3 place-items-center">
+          <ClearButton setInputUser={setInputUser} />
+          <Button input={"%"} setInputUser={setInputUser} />
+          <BackspaceButton setInputUser={setInputUser} />
+          <Button input={"/"} setInputUser={setInputUser} />
+
+          <Button input={"7"} setInputUser={setInputUser} />
+          <Button input={"8"} setInputUser={setInputUser} />
+          <Button input={"9"} setInputUser={setInputUser} />
+          <Button input={"+"} setInputUser={setInputUser} />
+
+          <Button input={"4"} setInputUser={setInputUser} />
+          <Button input={"5"} setInputUser={setInputUser} />
+          <Button input={"6"} setInputUser={setInputUser} />
+          <Button input={"-"} setInputUser={setInputUser} />
+
+          <Button input={"1"} setInputUser={setInputUser} />
+          <Button input={"2"} setInputUser={setInputUser} />
+          <Button input={"3"} setInputUser={setInputUser} />
+          <Button input={"×"} setInputUser={setInputUser} />
+
+          <Button input={<IconExpand />} />
+          <Button input={"0"} setInputUser={setInputUser} />
+          <EqualButton setInputUser={setInputUser} result={result} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Button = ({ input, setInputUser }) => {
+  return (
+    <button
+      onClick={() => {
+        const operators = ["+", "-", "×", "/"];
+
+        setInputUser((prev) => {
+          if (prev == "0") {
+            if (operators.includes(input) || input == "%") {
+              return "0";
+            } else {
+              return input;
+            }
+          } else {
+            if (
+              (operators.includes(prev.slice(-1)) &&
+                (input == "+" ||
+                  input == "×" ||
+                  input == "/" ||
+                  input == "%" ||
+                  input == "0")) ||
+              (operators.includes(prev.slice(-1)) &&
+                operators.includes(prev.slice(-2, -1)) &&
+                input == "-")
+            ) {
+              return prev;
+            } else {
+              return prev + input;
+            }
+          }
+        });
+      }}
+      className="w-full h-12 text-2xl rounded-md bg-slate-600 text-slate-300 cursor-pointer hover:bg-slate-700 transition-colors"
+    >
+      {input}
+    </button>
+  );
+};
+
+const ClearButton = ({ setInputUser }) => {
+  return (
+    <button
+      onClick={() => {
+        setInputUser("0");
+      }}
+      className="w-full h-12 text-2xl rounded-md bg-slate-600 text-slate-300 cursor-pointer hover:bg-slate-700 transition-colors"
+    >
+      C
+    </button>
+  );
+};
+
+const BackspaceButton = ({ setInputUser }) => {
+  return (
+    <button
+      onClick={() => {
+        setInputUser((prev) => {
+          if (prev.length == 1) {
+            return "0";
+          } else {
+            return prev.slice(0, -1);
+          }
+        });
+      }}
+      className="w-full h-12 text-2xl rounded-md bg-slate-600 text-slate-300 cursor-pointer hover:bg-slate-700 transition-colors"
+    >
+      <IconBackspace />
+    </button>
+  );
+};
+
+const EqualButton = ({ setInputUser, result }) => {
+  return (
+    <button
+      onClick={() => {
+        setInputUser(result.toString());
+      }}
+      className="w-full h-12 text-2xl col-span-2 rounded-md bg-orange-400 text-slate-300 cursor-pointer hover:bg-orange-500 transition-colors"
+    >
+      =
+    </button>
+  );
+};
+
+const IconExpand = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6 m-auto"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+      />
+    </svg>
+  );
+};
+const IconBackspace = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6 m-auto"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 9.75 14.25 12m0 0 2.25 2.25M14.25 12l2.25-2.25M14.25 12 12 14.25m-2.58 4.92-6.374-6.375a1.125 1.125 0 0 1 0-1.59L9.42 4.83c.21-.211.497-.33.795-.33H19.5a2.25 2.25 0 0 1 2.25 2.25v10.5a2.25 2.25 0 0 1-2.25 2.25h-9.284c-.298 0-.585-.119-.795-.33Z"
+      />
+    </svg>
+  );
+};
+
+export default Calculator;
