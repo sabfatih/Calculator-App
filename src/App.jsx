a7b9dde68c1737ddf2d37c937d377b93f06a810f
Fatih <alpha5808r@gmail.com>
2025-04-08 22:50:13 +0700

    Change the gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Calculator from "./components/Calculator";
 function App() {
   return (
     <>
+      <div className="-z-10 w-full min-h-screen absolute inset-0 bg-gradient-to-b from-slate-600 to-purple-700"></div>
       <header className="text-center my-5">
         <span className="text-5xl font-extrabold bg-gradient-to-l from-purple-400 to-fuchsia-400 bg-clip-text text-transparent">
           Calculator
